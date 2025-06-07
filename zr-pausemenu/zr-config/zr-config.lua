b03ae8266c53a639b534d6b5e8d76fc3210689be
Rysiu201 <54910862+Rysiu201@users.noreply.github.com>
2025-06-07 03:44:12 +0200

    Add files via upload

---
+++ zr-pausemenu/zr-config/zr-config.lua
@@ -0,0 +1,41 @@
+zr_config = {}
+zr_config.zr_translation = {}
+zr_trans = zr_config.zr_translation
+
+-- We support the following frameworks: 'QB' / 'ESX'
+zr_config.framework = 'QB'
+zr_config.QB = 'qb-core'
+zr_config.ESX = 'es_extended'
+
+zr_config.zr_blur = 'MenuMGIn'
+zr_config.disconnect = 'Ding dong ping pong!'
+zr_config.discord = 'https://discord.gg/9P6KtVNZyk'
+
+zr_config.MulticharacterLogout = false
+
+zr_config.jobs = {
+    {id='police', name='police', suivname='officers', color='#00A8FF'},
+    {id='ambulance', name='ems', suivname='doctors', color='#FF5D5D'},
+}
+
+zr_trans.discord        = 'Dołącz do naszego Discorda.'
+zr_trans.maps           = 'MAPA'
+zr_trans.mapstxt        = 'POKAŻ MAPĘ MIASTA.'
+zr_trans.settings       = 'USTAWIENIA'
+zr_trans.settingstxt    = 'OTWÓRZ MENU USTAWIEŃ GRY.'
+zr_trans.rulestitle     = 'ZASADY SERWERA'
+zr_trans.rules          = "1. Lorem Ipsum.<br>2. Lorem Ipsum.<br>3. Lorem Ipsum.<br>4. Lorem Ipsum.<br>5. Lorem Ipsum.<br>6. Lorem Ipsum.<br>7. Lorem Ipsum.<br>8. Lorem Ipsum.<br>9. Lorem Ipsum.<br>10. Lorem Ipsum."
+zr_trans.back           = 'Wróć do gry.'
+zr_trans.logout         = 'Wyloguj postać.'
+zr_trans.disconnect     = 'Rozłącz się z serwerem.'
+zr_trans.infostitle     = 'Informacje o postaci.'
+zr_trans.fname          = 'Imię'
+zr_trans.lname          = 'Nazwisko'
+zr_trans.gender         = 'Płeć'
+zr_trans.male           = 'MĘŻCZYZNA'
+zr_trans.female         = 'KOBIETA'
+zr_trans.dob            = 'Data Ur.'
+zr_trans.job            = 'Praca'
+zr_trans.cash           = 'Gotówka'
+zr_trans.bank           = 'Bank'
+zr_trans.currency       = '$' -- lub '$', zależnie od waluty na serwerze
