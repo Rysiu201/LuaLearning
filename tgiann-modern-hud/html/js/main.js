12cde97a100174e1c0a34316a9b45c748e707a6d
Rysiu201 <54910862+Rysiu201@users.noreply.github.com>
2025-06-11 18:43:07 +0200

    Mirror HUD on right side

--- tgiann-modern-hud/html/js/main.js
+++ tgiann-modern-hud/html/js/main.js
@@ -75,6 +75,23 @@ window.addEventListener("message", (event) => {
         JSON.stringify({ isCircle: switchcircle })
       );
 
+      let hudPos = window.localStorage.getItem("hudPosition") || "left";
+      if (hudPos === "right") {
+        $("body").addClass("hud-right");
+        $("#hudPosition").val("right");
+        $.post("https://tgiann-modern-hud/setHudPosition", JSON.stringify({ pos: "right" }));
+      } else {
+        $("body").addClass("hud-left");
+        $("#hudPosition").val("left");
+        $.post("https://tgiann-modern-hud/setHudPosition", JSON.stringify({ pos: "left" }));
+      }
+
+      let mono = window.localStorage.getItem("monochrome") === "true";
+      if (mono) {
+        $("body").addClass("mono");
+        $("#monochrome").prop("checked", true);
+      }
+
       break;
   }
   if (event.data.action == "hudmenu") {
@@ -263,7 +280,7 @@ window.addEventListener("message", (event) => {
     $(".kmh-number").html(event.data.speed)
   }
   else if (event.data.action == "talking") {
-    $(".microphoneMicrophone").css("color", "#fff");
+    $(".microphoneMicrophone").css("color", "#9400d3");
   }
   else if (event.data.action == "Nottalking") {
     $(".microphoneMicrophone").css("color", "rgba(255, 255, 255, 0.4)");
@@ -398,6 +415,28 @@ $(document).on("click", "#water", function (e) {
     } else {
       elem.fadeOut();
     }
+  }
+});
+
+$(document).on("click", "#monochrome", function (e) {
+  const on = e.currentTarget.checked;
+  window.localStorage.setItem("monochrome", on);
+  if (on) {
+    $("body").addClass("mono");
+  } else {
+    $("body").removeClass("mono");
+  }
+});
+
+$(document).on("change", "#hudPosition", function (e) {
+  const pos = e.currentTarget.value;
+  window.localStorage.setItem("hudPosition", pos);
+  if (pos === "right") {
+    $("body").removeClass("hud-left").addClass("hud-right");
+    $.post("https://tgiann-modern-hud/setHudPosition", JSON.stringify({ pos: "right" }));
+  } else {
+    $("body").removeClass("hud-right").addClass("hud-left");
+    $.post("https://tgiann-modern-hud/setHudPosition", JSON.stringify({ pos: "left" }));
   }
 });
 
