d41555001bc328acfaa13c694266796b1a1f3b65
Rysiu201 <choroba000@icloud.com>
2025-06-21 01:39:25 +0200

    merge to prod

---
+++ qb_trew_hud/config.lua
@@ -0,0 +1,79 @@
+Config = {}
+
+Config.Locale = 'pl'
+
+Config.serverLogo = 'https://cdn.discordapp.com/attachments/1119221551657648168/1129015526270717982/Yeni_Proje_3.png?ex=65969a25&is=65842525&hm=3798d63d5850f0798833193b03e67352b97588827c2465031472716be6db4c24&'
+
+Config.font = {
+	name 	= 'Signika Negative',
+	url 	= 'https://fonts.googleapis.com/css2?family=Signika+Negative&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= true
+}
+
+Config.vehicle = {
+        speedUnit = 'MPH',
+        maxSpeed = 175,
+
+	keys = {
+		seatbelt 	= 'b',
+		cruiser		= 'Y',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+        }
+}
+
+-- List of electric vehicle model names to show battery icon
+Config.electricVehicles = {
+        "voltic",
+        "voltic2",
+        "caddy",
+        "caddy2",
+        "caddy3",
+        "surge",
+        "iwagen",
+        "raiden",
+        "airtug",
+        "neon",
+        "omnisegt",
+        "cyclone",
+        "tezeract",
+        "rcbandito",
+        "imorgon",
+        "dilettante",
+        "dilettante2",
+        "khamelion",
+}
+
+Config.ui = {
+	showServerLogo		= false,
+
+	showJob		 		= false,
+
+	showWalletMoney 	= false,
+	showBankMoney 		= false,
+	showCrypto 			= false,
+	showSocietyMoney	= false,
+
+	showDate 			= false,
+	showLocation 		= false,
+	showVoice	 		= false,
+
+	showHealth			= false,
+	showArmor	 		= false,
+	showStamina	 		= false,
+	showHunger 			= false,
+	showThirst	 		= false,
+	showStress	 		= false,
+	showID	 			= false,
+
+	showMinimapOnFoot 	 = false,
+	showMinimapInVehicle = true,
+	showMinimapHealth    = false,
+
+	showWeapons			= false,	
+}
