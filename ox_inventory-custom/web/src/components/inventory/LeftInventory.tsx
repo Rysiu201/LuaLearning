832b557b5f45773a70e5c7522801f5889af406d5
Rysiu201 <54910862+Rysiu201@users.noreply.github.com>
2025-06-28 03:24:26 +0200

    feat: modernize pockets container

--- ox_inventory-custom/web/src/components/inventory/LeftInventory.tsx
+++ ox_inventory-custom/web/src/components/inventory/LeftInventory.tsx
@@ -1,13 +1,37 @@
 import InventoryGrid from './InventoryGrid';
 import { useAppSelector } from '../../store';
 import { selectLeftInventory } from '../../store/inventory';
+import { useMemo } from 'react';
+import { getTotalWeight } from '../../helpers';
 
 const LeftInventory: React.FC = () => {
   const leftInventory = useAppSelector(selectLeftInventory);
 
+  const weight = useMemo(
+    () =>
+      leftInventory.maxWeight !== undefined
+        ? Math.floor(getTotalWeight(leftInventory.items) * 1000) / 1000
+        : 0,
+    [leftInventory.maxWeight, leftInventory.items]
+  );
+
   return (
     <div className="left-inventory">
-      <h2 className="pockets-title">Pockets</h2>
+      <div className="pockets-header">
+        <span className="pockets-title">Pockets</span>
+        {leftInventory.maxWeight && (
+          <span className="pockets-weight">
+            {(weight / 1000).toLocaleString('en-us', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+            /{(leftInventory.maxWeight / 1000).toLocaleString('en-us', {
+              minimumFractionDigits: 0,
+            })}
+            kg
+          </span>
+        )}
+      </div>
       <InventoryGrid inventory={leftInventory} />
     </div>
   );
