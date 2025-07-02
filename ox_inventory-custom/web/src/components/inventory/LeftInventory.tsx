c37e9052f51108d5ce2df2d2e1201bd6bf001646
Rysiu201 <54910862+Rysiu201@users.noreply.github.com>
2025-07-02 20:15:53 +0200

    Add collapsible pockets inventory

--- ox_inventory-custom/web/src/components/inventory/LeftInventory.tsx
+++ ox_inventory-custom/web/src/components/inventory/LeftInventory.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import InventoryGrid from './InventoryGrid';
 import { useAppSelector } from '../../store';
 import { selectPocketsInventory } from '../../store/inventory';
 
 const LeftInventory: React.FC = () => {
   const leftInventory = useAppSelector(selectPocketsInventory);
+  const [collapsed, setCollapsed] = useState(false);
 
   return (
     <div className="left-inventory">
       <h2 className="pockets-title">Pockets</h2>
-      <InventoryGrid inventory={leftInventory} showSlotNumbers={false} />
+      <InventoryGrid
+        inventory={leftInventory}
+        showSlotNumbers={false}
+        collapsible
+        collapsed={collapsed}
+        onToggleCollapse={() => setCollapsed(!collapsed)}
+      />
     </div>
   );
 };
